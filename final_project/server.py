c2f77367a0c4ff901b7a83501be2ae9605e268f7
kilowiski <kilocharlie246@protonmail.com>
2022-02-14 14:12:33 +0800

    fix server.py

--- final_project/server.py
+++ final_project/server.py
@@ -7,17 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    enfr=translator.english_to_french(textToTranslate)
+    return enfr
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    fren=translator.french_to_english(textToTranslate)
+    return fren
 
 @app.route("/")
 def renderIndexPage():
+    return render_template("index.html")
     # Write the code to render template
 
 if __name__ == "__main__":
